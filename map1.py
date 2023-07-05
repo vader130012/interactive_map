e4d31110f9db31b7b1ed4c73d156283923a0bca1
vader130012 <91274240+vader130012@users.noreply.github.com>
2023-07-05 15:37:54 -0400

    Update map1.py
    removed folium libarary and related functions

--- map1.py
+++ map1.py
@@ -9,8 +9,8 @@
 
 import streamlit as st
 import pandas as pd
-import folium
-from streamlit_folium import st_folium
+# import folium
+# from streamlit_folium import st_folium
 
 # to set app title / nav bar text
 app_title = 'Alberta Approved Farmers Market 2023'
@@ -27,31 +27,31 @@ st.write('CSV file pollted on a map with streamlit map function')
 st.map(df_farmarket)
 
 # map2
-st.write('Folium maps in streamlit')
+# st.write('Folium maps in streamlit')
 
-m = folium.Map(location=[df_farmarket.latitude.mean(), df_farmarket.longitude.mean()],
-               zoom_start=5,
-               control_scale=True,
-               tiles="Stamen Terrain")
+# m = folium.Map(location=[df_farmarket.latitude.mean(), df_farmarket.longitude.mean()],
+#                zoom_start=5,
+#                control_scale=True,
+#                tiles="Stamen Terrain")
 
-tooltip = "click me!" # text to show up once mouse is over marker
+# tooltip = "click me!" # text to show up once mouse is over marker
 
-# here we use for loop to go over each row from the table and make a marker on lat and long.
-for i,row in df_farmarket.iterrows():
-    iframe = folium.IFrame('Market ID:'+ str(row["Market ID"])+ '<br>'+
-                           'Name:'+str(row["Venue Name"]) + '<br>'+ 
-                           'Address:'+str(row["Venue Address"]) + '<br>'+
-                           'Contact:'+str(row["Contact"]) + '<br>'+
-                           'Website:'+str(row["Website"]) + '<br>'+
-                           'Town:'+str(row["Town"]) + '<br>'+
-                           'Schedule:'+str(row["Market Schedule"]))
+# # here we use for loop to go over each row from the table and make a marker on lat and long.
+# for i,row in df_farmarket.iterrows():
+#     iframe = folium.IFrame('Market ID:'+ str(row["Market ID"])+ '<br>'+
+#                            'Name:'+str(row["Venue Name"]) + '<br>'+ 
+#                            'Address:'+str(row["Venue Address"]) + '<br>'+
+#                            'Contact:'+str(row["Contact"]) + '<br>'+
+#                            'Website:'+str(row["Website"]) + '<br>'+
+#                            'Town:'+str(row["Town"]) + '<br>'+
+#                            'Schedule:'+str(row["Market Schedule"]))
 
-    popup = folium.Popup(iframe,max_width=300, min_width = 300)
-    folium.Marker(location=[row['latitude'],row['longitude']],
-                  tooltip=tooltip,
-                  popup=popup).add_to(m)
+#     popup = folium.Popup(iframe,max_width=300, min_width = 300)
+#     folium.Marker(location=[row['latitude'],row['longitude']],
+#                   tooltip=tooltip,
+#                   popup=popup).add_to(m)
     
-st_data = st_folium(m,width=700)
+# st_data = st_folium(m,width=700)
 
 # map3
 
